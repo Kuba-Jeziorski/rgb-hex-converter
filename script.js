e294ed17c40ae9275168d284368e1415c13686fc
Jakub Jeziorski <106917056+Kuba-Jeziorski@users.noreply.github.com>
2022-08-31 21:15:09 +0200

    converter

---
+++ script.js
@@ -0,0 +1,114 @@
+"use strict";
+
+const red = document.getElementById("red");
+const green = document.getElementById("green");
+const blue = document.getElementById("blue");
+const hex = document.getElementById("hex");
+const rgbH = document.getElementById("rgb-to-hex");
+const hexR = document.getElementById("hex-to-rgb");
+const rgbHC = document.getElementById("rgb-to-hex-color");
+const hexRC = document.getElementById("hex-to-rgb-color");
+const alpha = document.getElementById("alpha");
+const rgbHCopy = document.getElementById("hex-to-rgb-copy");
+const hexRCopy = document.getElementById("rgb-to-hex-copy");
+
+const valueMap = new Map([
+  [10, "A"],
+  [11, "B"],
+  [12, "C"],
+  [13, "D"],
+  [14, "E"],
+  [15, "F"],
+]);
+const valueMap2 = new Map([
+  ["A", 10],
+  ["B", 11],
+  ["C", 12],
+  ["D", 13],
+  ["E", 14],
+  ["F", 15],
+]);
+
+const contentCopy = function (content) {
+  navigator.clipboard.writeText(content);
+};
+
+document
+  .getElementById("conversion-form")
+  .addEventListener("submit", function (e) {
+    e.preventDefault();
+    let arrF = [];
+    let arr1 = [];
+    let arr2 = [];
+    let arr3 = [];
+    let whole = [];
+
+    alpha.value =
+      alpha.value < 1 && alpha.value >= 0.01 ? alpha.value * 100 : alpha.value;
+
+    //// RGB -> HEX
+    const converterRgbHex = function (color) {
+      const value1 = Math.floor(color / 16);
+      const value2 = (color / 16 - value1) * 16;
+
+      let value1C = value1 < 10 ? value1.toString() : valueMap.get(value1);
+      let value2C = value2 < 10 ? value2.toString() : valueMap.get(value2);
+
+      arrF = [value1C, value2C];
+      return arrF;
+    };
+
+    arr1 = converterRgbHex(red.value);
+    arr2 = converterRgbHex(green.value);
+    arr3 = converterRgbHex(blue.value);
+    whole = [...arr1, ...arr2, ...arr3].join().replaceAll(",", "");
+    rgbH.innerHTML =
+      alpha.value === "" ? `#${whole}` : `#${whole}${alpha.value}`;
+
+    //// HEX -> RGB
+    const converterHexRgb = function (string) {
+      string = string.includes("#") ? string.replaceAll("#", "") : string;
+
+      string = string.toUpperCase();
+      let array = string.split("");
+
+      for (let i = 0; i < array.length; i++) {
+        array[i] =
+          array[i] < 10 ? array[i].toString() : valueMap2.get(array[i]);
+        array[i] = Number(array[i]);
+      }
+
+      let hexR =
+        (array[0] + array[1] / 16) * 16 > 255
+          ? "too much"
+          : (array[0] + array[1] / 16) * 16;
+      let hexG =
+        (array[2] + array[3] / 16) * 16 > 255
+          ? "too much"
+          : (array[2] + array[3] / 16) * 16;
+      let hexB =
+        (array[4] + array[5] / 16) * 16 > 255
+          ? "too much"
+          : (array[4] + array[5] / 16) * 16;
+
+      let hexRGB = `${hexR}, ${hexG}, ${hexB}`;
+      return hexRGB;
+    };
+    hexR.innerHTML =
+      alpha.value === ""
+        ? `rgb(${converterHexRgb(hex.value)})`
+        : `rgb(${converterHexRgb(hex.value)}, ${alpha.value})`;
+
+    rgbHC.style.backgroundColor = `#${whole}`;
+    hexRC.style.backgroundColor = hexR.innerHTML;
+
+    hexRCopy.innerHTML = "KOPIUJ";
+    rgbHCopy.innerHTML = "KOPIUJ";
+
+    hexRCopy.addEventListener("click", function () {
+      contentCopy(rgbH.textContent);
+    });
+    rgbHCopy.addEventListener("click", function () {
+      contentCopy(hexR.textContent);
+    });
+  });
